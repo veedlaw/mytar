2d8c3e4504c6b4c47055de33955ca0ba9c84545b
Walter Herold Veedla <walterheroldv@gmail.com>
2022-05-27 14:32:34 +0200

    Reformat file

--- mytar.c
+++ mytar.c
@@ -6,33 +6,33 @@
 /* tar Header Block, from POSIX 1003.1-1990.  */
 /* POSIX header.  */
 struct posix_header
-{                              /* byte offset */
-  char name[100];               /*   0 */
-  char mode[8];                 /* 100 */
-  char uid[8];                  /* 108 */
-  char gid[8];                  /* 116 */
-  char size[12];                /* 124 */
-  char mtime[12];               /* 136 */
-  char chksum[8];               /* 148 */
-  char typeflag;                /* 156 */
-  char linkname[100];           /* 157 */
-  char magic[6];                /* 257 */
-  char version[2];              /* 263 */
-  char uname[32];               /* 265 */
-  char gname[32];               /* 297 */
-  char devmajor[8];             /* 329 */
-  char devminor[8];             /* 337 */
-  char prefix[155];             /* 345 */
-                                /* 500 */
+{						/* byte offset */
+	char name[100];		/*   0 */
+	char mode[8];		/* 100 */
+	char uid[8];		/* 108 */
+	char gid[8];		/* 116 */
+	char size[12];		/* 124 */
+	char mtime[12];		/* 136 */
+	char chksum[8];		/* 148 */
+	char typeflag;		/* 156 */
+	char linkname[100]; /* 157 */
+	char magic[6];		/* 257 */
+	char version[2];	/* 263 */
+	char uname[32];		/* 265 */
+	char gname[32];		/* 297 */
+	char devmajor[8];	/* 329 */
+	char devminor[8];	/* 337 */
+	char prefix[155];	/* 345 */
+						/* 500 */
 };
 char gnu_magic[] = "ustar  ";
 char ustar_magic[] = "ustar";
 
 /* Values used in typeflag field.  */
-#define REGTYPE  '0'            /* regular file */
+#define REGTYPE '0' /* regular file */
 size_t BLOCKSIZE = 512;
 
-enum Option 
+enum Option
 {
 	NO_OPT,
 	EXTRACT_OPT,
@@ -43,11 +43,11 @@ enum Option
  * @brief Returns cmd-line arguments.
  *
  * Walks the given pointer and returns each string.
- * 
+ *
  * @param argv Command-line arguments.
  * @return Argument or character of option or NULL at end of input.
  */
-char* get_arg(char*** argv)
+char *get_arg(char ***argv)
 {
 	while (NULL != **argv)
 	{
@@ -71,69 +71,68 @@ char* get_arg(char*** argv)
 struct tar_action_s
 {
 	enum Option option;
-	char* filename;
-	char** file_list;
+	char *filename;
+	char **file_list;
 	int file_list_len;
 	int verbose;
 };
 
 /**
  * @brief Parses the command-line arguments into a tar_action_s struct.
- * 
+ *
  * Walks the argv pointer to parse and check validity of command-line arguments.
  * If succesful then returns a tar_action_s, otherwise exits with an error.
- * 
+ *
  * @param argv Command-line arguments.
  * @return Struct describing action of parsed command-line arguments.
  */
-struct tar_action_s parse_args(int argc, char** argv)
+struct tar_action_s parse_args(int argc, char **argv)
 {
 	struct tar_action_s tar_action = {
 		.option = NO_OPT,
 		.filename = NULL,
-		.file_list = calloc(argc, sizeof(char*)),
+		.file_list = calloc(argc, sizeof(char *)),
 		.file_list_len = argc,
-		.verbose = 0
-	};
+		.verbose = 0};
 
-	if (argc == 1) 
+	if (argc == 1)
 	{
 		errx(2, "need at least one option");
 	}
 
 	int num_file = 0;
-	char* arg;
+	char *arg;
 	while ((arg = get_arg(&argv)) != NULL)
 	{
 		if (arg[0] == '-')
 		{
 			switch (arg[1])
 			{
-				case 't':
-					tar_action.option = LIST_OPT;
-					tar_action.verbose = 1;
-					break;
-				case 'x':
-					tar_action.option = EXTRACT_OPT;
-					break;
-				case 'f':
-					/**
-					 * If it is specified then the next word in
-					 * arguments is the filename.
-					 */
-					argv++;
-					if (NULL == *argv)
-					{
-						err(64, "option requires an argument -- 'f'");
-					}
-					tar_action.filename = *argv;
-					break;
-				case 'v':
-					tar_action.verbose = 1;
-					break;
-				default:
-					errx(2, "Unknown option: %s", arg);
-					break;
+			case 't':
+				tar_action.option = LIST_OPT;
+				tar_action.verbose = 1;
+				break;
+			case 'x':
+				tar_action.option = EXTRACT_OPT;
+				break;
+			case 'f':
+				/**
+				 * If it is specified then the next word in
+				 * arguments is the filename.
+				 */
+				argv++;
+				if (NULL == *argv)
+				{
+					err(64, "option requires an argument -- 'f'");
+				}
+				tar_action.filename = *argv;
+				break;
+			case 'v':
+				tar_action.verbose = 1;
+				break;
+			default:
+				errx(2, "Unknown option: %s", arg);
+				break;
 			}
 		}
 		else
@@ -143,7 +142,7 @@ struct tar_action_s parse_args(int argc, char** argv)
 		}
 	}
 
-	if (NULL != tar_action.filename && tar_action.option == NO_OPT) 
+	if (NULL != tar_action.filename && tar_action.option == NO_OPT)
 	{
 		errx(2, "You must specify one of the '-tx' options");
 	}
@@ -152,20 +151,18 @@ struct tar_action_s parse_args(int argc, char** argv)
 		errx(2, "a");
 	}
 
-
 	return tar_action;
 }
-
 
 /**
  * @brief Convert an octal string to a long in decimal format.
  * @param oct_str String representing octal number.
  * @return Decimal value of the octal number in string representation.
  */
-long oct_to_dec(char* oct_str)
+long oct_to_dec(char *oct_str)
 {
 	long result = 0, divisor = 1;
-	for (int i = strlen(oct_str)-1; i >= 0; i--) 
+	for (int i = strlen(oct_str) - 1; i >= 0; i--)
 	{
 		result += (oct_str[i] - '0') * divisor;
 		divisor *= 8;
@@ -177,7 +174,7 @@ long oct_to_dec(char* oct_str)
  * @brief Checks if BLOCKSIZE bytes of block are the null byte
  * @return 0 if true -1 otherwise.
  */
-int is_empty(char* block)
+int is_empty(char *block)
 {
 	for (size_t i = 0; i < BLOCKSIZE; i++)
 	{
@@ -196,17 +193,17 @@ int is_empty(char* block)
  * @param num_zero_blocks Count of blocks with zero values
  * @return 0 on success, -1 if recoverable error, otherwise exit with error.
  */
-int fetch_header(FILE* fp, struct posix_header* header, int* num_zero_blocks)
+int fetch_header(FILE *fp, struct posix_header *header, int *num_zero_blocks)
 {
 	// Read the header
-	size_t blocks_read = fread(header, 1, BLOCKSIZE, fp);  
-	
+	size_t blocks_read = fread(header, 1, BLOCKSIZE, fp);
+
 	if (blocks_read == 0)
 	{
 		return -1;
 	}
 	// Reading a block of 0's signals end of archive
-	if (is_empty((char*)header))
+	if (is_empty((char *)header))
 	{
 		(*num_zero_blocks)++;
 		return -1;
@@ -232,15 +229,15 @@ int fetch_header(FILE* fp, struct posix_header* header, int* num_zero_blocks)
  * @param file_list List of file names
  * @param file_list_len Length of file_list
  */
-void show_filename(char* filename, char** file_list, size_t file_list_len)
+void show_filename(char *filename, char **file_list, size_t file_list_len)
 {
 	// If the user has not specified file arguments then we always print the filename.
 	if (*file_list == NULL)
 	{
 		printf("%s\n", filename);
-		fflush(stdout);  
+		fflush(stdout);
 		return;
-	}	
+	}
 
 	// Otherwise, the user has specified some files and we check whether the current file
 	// is one that the user is specified.
@@ -252,7 +249,7 @@ void show_filename(char* filename, char** file_list, size_t file_list_len)
 			// Remove the filename so we know we have found it already.
 			file_list[i] = "";
 			printf("%s\n", filename);
-			fflush(stdout); // otherwise warnings get printed before as stderr is unbuffered. 
+			fflush(stdout); // otherwise warnings get printed before as stderr is unbuffered.
 		}
 	}
 }
@@ -261,7 +258,7 @@ void show_filename(char* filename, char** file_list, size_t file_list_len)
  * @brief Convert octal tar header size into number of blocks until end of file.
  * @return Number of BLOCKSIZE blocks until end of the file
  */
-long filesize_to_block_count(char* size)
+long filesize_to_block_count(char *size)
 {
 	long size_in_bytes = oct_to_dec(size);
 	long num_file_blocks = size_in_bytes / BLOCKSIZE;
@@ -272,37 +269,36 @@ long filesize_to_block_count(char* size)
 	return num_file_blocks;
 }
 
-
 /**
  * @brief List the contents of a tar archive.
  * Exits with an error if something goes wrong.
  */
 void op_on_archive(struct tar_action_s operations)
-{	
-	char* archive_name = operations.filename;
-	char** file_list = operations.file_list;
+{
+	char *archive_name = operations.filename;
+	char **file_list = operations.file_list;
 	size_t file_list_len = operations.file_list_len;
 
-	FILE* fp;
-	FILE* fout;
+	FILE *fp;
+	FILE *fout;
 	if ((fp = fopen(archive_name, "r")) == NULL)
 	{
 		errx(2, "%s: Cannot open: No such file or directory\nError is not recoverable: exiting now", archive_name);
 	}
-	
+
 	// Allocate memory for the header
-	struct posix_header* header = malloc(sizeof(struct posix_header)); 
-	char* block = malloc(BLOCKSIZE*sizeof(char));
+	struct posix_header *header = malloc(sizeof(struct posix_header));
+	char *block = malloc(BLOCKSIZE * sizeof(char));
 	if (header == NULL || block == NULL)
 	{
 		errx(2, "Unable to allocate memory to read archive. Error is not recoverable: exiting now");
 	}
-	
+
 	size_t blocks_read = 0;
 	int num_zero_blocks = 0;
 
 	// Repeat for each valid header we read:
-	while(fetch_header(fp, header, &num_zero_blocks) == 0)
+	while (fetch_header(fp, header, &num_zero_blocks) == 0)
 	{
 		if (operations.verbose)
 		{
@@ -344,7 +340,7 @@ void op_on_archive(struct tar_action_s operations)
 			}
 		}
 	}
-	
+
 	// A tar archive should end with two zero-blocks.
 	// Read a block to check that condition holds
 	blocks_read = fread(block, BLOCKSIZE, 1, fp);
@@ -377,8 +373,7 @@ void op_on_archive(struct tar_action_s operations)
 	// free(block);
 }
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	// Get the parsed form of the command line arguments.
 	struct tar_action_s operations = parse_args(argc, argv);
